b9f42e319f5bdc9098141e73e8beb9e0169c08b8
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-07 09:31:00 +0100

    Add expectations tests (avoiding use case in comments)

--- src/clj_monad_lab/monad.clj
+++ src/clj_monad_lab/monad.clj
@@ -1,44 +1,52 @@
 (ns clj-monad-lab.monad
-  "Learn to dry my understanding of monads. Implement something `'equivalent` in clojure'")
+  "Learn to dry my understanding of monads. Implement something `'equivalent` in clojure'"
+  (:require [expectations :as t]))
 
 ;; functor
 ;; fmap :: (a -> b) -> f a -> f b
 
 ;; Monad
 
+;; (>>= nil        _)           <=> nil
+;; (>>= (return a) identity)    <=> (return a)
+;; (>>= (return a) (fn [a] ...) <=> (return ((fn [a] ...) a))
+
 (defn return "a -> m a"
   [a]
   {:value a})
 
+(t/expect {:value 10} (return 10))
+(t/expect {:value [1 2 3]} (return [1 2 3]))
+
 (defn fail "Deal with failure"
   [msg]
   (return msg))
+
+(t/expect {:value "error message"} (fail "error message"))
 
 (defn >>= "Monad m => m a -> (a -> m b) -> m b"
   [{:keys [value] :as monad-value} a->mb]
   (try (when monad-value (a->mb value))
        (catch java.lang.Exception e (fail "Error"))))
 
-;; (>>= nil        _)           <=> nil
-;; (>>= (return a) identity)    <=> (return a)
-;; (>>= (return a) (fn [a] ...) <=> (return ((fn [a] ...) a))
+;; value
+(t/expect {:value 10}
+          (>>= (return 10) (comp identity return)))
 
-;; clj-monad-lab.monad> (>>= (return 10) (comp identity return))
-;; {:value 10}
-;; clj-monad-lab.monad> (>>= (return 10) #(return (+ 100 %)))
-;; {:value 110}
+(t/expect {:value 110}
+          (>>= (return 10) #(return (+ 100 %))))
 
-;; clj-monad-lab.monad> (>>= (>>= (>>= {:value 10} #(return (* 2 %))) #(return (+ 1 %))) #(return (- 10 %)))
-;; {:value -11}
+(t/expect {:value -11}
+          (>>= (>>= (>>= {:value 10} #(return (* 2 %))) #(return (+ 1 %))) #(return (- 10 %))))
 
-;; it would be better in infix notation
-;; {:value 10} >>= #(return (* 2 %)) >>= #(return (+ 1 %)) >>= #(return (- 10 %))
-
-;; clj-monad-lab.monad> (>>= (>>= (>>= nil #(return (* 2 %))) #(return (+ 1 %))) #(return (- 10 %)))
 ;; nil
+(t/expect nil
+          (>>= (>>= (>>= nil #(return (* 2 %))) #(return (+ 1 %))) #(return (- 10 %))))
+(t/expect nil
+          (>>= (>>= (>>= nil #(return (* 2 %))) #(return (+ 1 %))) #(return (- 10 %))))
 
-;; clj-monad-lab.monad> (>>= (>>= (>>= {:value 10} #(return (* 2 %))) (constantly nil)) #(return (- 10 %)))
-;; nil
+;; exception
+(t/expect nil
+          (>>= (>>= (>>= {:value 10} #(return (* 2 %))) (constantly nil)) #(return (- 10 %))))
 
-;; clj-monad-lab.monad> (>>= (>>= (>>= {:value 10} #(return (* 2 %))) (fn [_] (/ 1 0))) #(return (- 10 %)))
-;; {:value "Error"}
+(t/expect {:value "Error"} (>>= (>>= (>>= {:value 10} #(return (* 2 %))) (fn [_] (/ 1 0))) #(return (- 10 %))))
